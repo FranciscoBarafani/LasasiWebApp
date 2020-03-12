8bbfe228dc231956ad43cbdd116cee2b8cc78987
Francisco  Barafani <franciscobarafani@Franciscos-MacBook-Pro.local>
2020-03-12 10:18:51 -0300

    Updated calendar component

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Calendar.scss";
 
-export default function MyCalendar(source) {
+export default function MyCalendar(props) {
+  const { source } = props;
   return (
     <div className="calendar">
       <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FArgentina%2FBuenos_Aires&amp;src=ZXMudXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%230B8043&amp;showTitle=0"
+        src={source}
         width="800"
         height="600"
         frameborder="0"
--- src/pages/events/events.js
+++ src/pages/events/events.js
@@ -7,7 +7,7 @@ export default function Events() {
   return (
     <div className="events">
       <h2>Calendario de Eventos</h2>
-      <MyCalendar />
+      <MyCalendar source="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FArgentina%2FBuenos_Aires&amp;src=ZXMudXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%230B8043&amp;showTitle=0" />
     </div>
   );
 }
