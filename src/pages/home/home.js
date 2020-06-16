73dad2c6eab2a42dd480353b78e13792e6378f66
Francisco Barafani <franbarafani@gmail.com>
2020-06-16 11:46:10 -0300

    Layout Fixes
    -Deleted Hooks folder, not being used anymore
    -Deleted not used images in asses

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,18 +2,14 @@
 import React from "react";
 import "./home.scss";
 //Components
-import ImageBox from "../../components/ImageBox";
 import HomeText from "../../components/HomeText";
 import { Col, Row } from "antd";
-//Data
-import boeingImage from "../../assets/images/boeing-787.jpeg";
 
 export default function Home() {
   return (
     <div className="home">
       <Row>
         <Col span={16} offset={4}>
-          <ImageBox alt="Imagen" image={boeingImage} />
           <div className="presentation">
             <HomeText />
           </div>
